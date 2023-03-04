e9db4791f9d656bd33d8467a8d7bf278e74d7cde
geek-nath <nath.codes@gmail.com>
2023-03-04 14:50:47 +0000

    project completed

--- assets/Js/app.js
+++ assets/Js/app.js
@@ -1,1 +1,18 @@
-let count = 0;
+let counter = 0;
+let increamentBtn = document.getElementById('increamentBtn');
+let countEl = document.getElementById('count_el');
+
+increamentBtn.addEventListener('click', ()=> {
+    counter++;
+
+    if(counter >= 1) {
+        countEl.innerHTML = counter;
+    }
+});
+
+// refresh tab onclik
+const refreshBtn = document.getElementById('refreshBtn');
+
+refreshBtn.onclick = ()=> {
+    location.reload();
+}
